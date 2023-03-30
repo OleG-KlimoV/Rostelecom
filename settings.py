d2bfa7c45829334e72d845f756fa86bb0b0ede39
OleG-KlimoV <122331966+OleG-KlimoV@users.noreply.github.com>
2023-03-30 10:00:51 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,11 @@
+my_valid_email = "hojavij597@usharercom"
+my_email_pass = "Zxc13579"
+
+my_valid_phone = "+79166513808"
+my_phone_pass = "Zxc13579"
+my_phone_pass_vk = "Q#22334455w"
+
+rand_email = "y37c7hx1mm@dcctb.com"
+rand_email_pass = "8kf-T7C-dLK-HEE"
+rand_phone = "+79991112233"
+rand_phone_pass = "9yDTn9Yqi#"
